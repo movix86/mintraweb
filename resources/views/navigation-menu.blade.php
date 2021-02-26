218d9a0d20e453509fbb18f06e018b879b249fd9
movix86 <movix86@gmail.com>
2021-02-25 21:32:55 -0500

    Se comento el logotipo de navigation-menu.blade para posterior edicion

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,7 +6,8 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                        <x-jet-application-mark class="block h-9 w-auto"/>
+                        <!-- Logotipo que aparece en la dashboard -->
                     </a>
                 </div>
 
